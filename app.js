250f52fbe775bc7651094ebb488097b100a5b0ba
nmerk <nadyamerk@gmail.com>
2019-10-29 16:51:58 +0300

    metrics done

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
-var express = require('express');
-var path = require('path');
-// var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const client = require('prom-client');
+const apiMetrics = require('prometheus-api-metrics');
 
-var indexRouter = require('./routes/index');
+const router = express();
+const routerMetrics = express();
 
-var app = express();
+const OK = 200;
+const NOT_OK = 500;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+let serverResponseOK = true;
 
-app.use('/', indexRouter);
+const metricsRequest = new client.Counter({
+    name: 'request',
+    help: 'kek',
+    labelNames: ['req200', 'req500'],
+});
 
-module.exports = app;
+const registry = new client.Registry();
+registry.registerMetric(metricsRequest);
+
+routerMetrics.use(apiMetrics());
+
+router.get('/api/ok', function(req, res, next) {
+    metricsRequest.inc(serverResponseOK ? {'req200': '/api/ok'} : {'req500': '/api/ok'}, 1);
+    res.sendStatus(serverResponseOK ? OK : NOT_OK);
+    serverResponseOK = true;
+});
+
+router.get('/api/notok', function(req, res, next) {
+    metricsRequest.inc(serverResponseOK ? {'req200': '/api/notok'} : {'req200': '/api/notok'}, 1);
+    res.sendStatus(serverResponseOK ? OK : NOT_OK);
+    serverResponseOK = false;
+});
+
+router.get('/api/status', function(req, res, next) {
+    res.sendStatus(serverResponseOK ? OK : NOT_OK);
+});
+
+router.get('/api/data', function(req, res, next) {
+    if (serverResponseOK) {
+        setTimeout(() => {
+            const data = {
+                string: 'Request paused for 2 seconds',
+                status: OK
+            };
+            metricsRequest.inc(serverResponseOK ? {'req200': '/api/data'} : {'req500': '/api/data'}, 1);
+            res.send(JSON.stringify(data));
+        }, 2000);
+    } else {
+        setTimeout(() => {
+            const data = {
+                string: 'Request paused for 10 seconds',
+                status: NOT_OK
+            };
+            metricsRequest.inc(serverResponseOK ? {'req200': '/api/notok'} : {'req500': '/api/notok'}, 1);
+            res.send(JSON.stringify(data));
+        }, 10000);
+    }
+});
+
+router.listen(80);
+routerMetrics.listen(8080);
